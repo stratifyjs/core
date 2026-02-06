003ce8039496cb09beb6fa1ed404dc82346c9cec
Jean <110341611+jean-michelet@users.noreply.github.com>
2026-02-06 18:24:27 +0100

    chore: upgrade dependencies (#29)

--- src/fastify/adapters.integration.test.ts
+++ src/fastify/adapters.integration.test.ts
@@ -47,7 +47,7 @@ describe("adapters integration", () => {
     const json = res.json();
     t.assert.deepStrictEqual(json, {
       ok: true,
-      msg: "5.6.2",
+      msg: "5.7.4",
     });
 
     await app.close();
